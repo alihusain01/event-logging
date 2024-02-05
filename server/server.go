83490d00230e414215dc5fcb944da7136c07a350
alijh2 <alijh2@sp24-cs425-4201.cs.illinois.edu>
2024-02-05 16:56:10 -0600

    finished writing to output file

--- server/server.go
+++ server/server.go
@@ -12,32 +12,23 @@ import (
 
 var (
 	mu      sync.Mutex
+	fileMu  sync.Mutex
 	clients = make(map[net.Conn]string) // Map to store connections and associated node names
+	logFile *os.File
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	// Lock to safely access the clients map
-	mu.Lock()
-	nodeName, ok := clients[conn]
-	mu.Unlock()
-
-	if !ok {
-		fmt.Println("Error: Node name not found for connection.")
-		return
-	}
-
-	// Read and print the initial connection message
-	netData, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Print("-> ", string(netData))
-
 	for {
 		netData, err := bufio.NewReader(conn).ReadString('\n')
+
+		// Parse the input into nodename and event timestamp
+		parts := strings.Split(netData, " ")
+		event_timestamp := parts[0]
+		nodeName := parts[1]
+		messageSize := len(netData)
+
 		if err != nil {
 			fmt.Printf("%s - %s disconnected\n", timestamp(), nodeName)
 			// Lock to safely remove the disconnected node from the clients map
@@ -46,6 +37,7 @@ func handleConnection(conn net.Conn) {
 			mu.Unlock()
 			return
 		}
+
 
 		if strings.TrimSpace(string(netData)) == "STOP" {
 			fmt.Printf("%s - %s disconnected\n", timestamp(), nodeName)
@@ -56,10 +48,20 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 
-		fmt.Print("-> ", string(netData))
+		fmt.Printf("-> %s \n", string(netData))
+		logEntry := fmt.Sprintf("%s %s %d \n", timestamp(), event_timestamp, messageSize)
+
+		fileMu.Lock()
+		_, err = logFile.WriteString(logEntry)
+		if err != nil {
+			fmt.Println("Error writing to log file:", err)
+		}
+		fileMu.Unlock()
+
 		t := time.Now()
 		myTime := t.Format(time.RFC3339) + "\n"
 		conn.Write([]byte(myTime))
+
 	}
 }
 
@@ -104,7 +106,23 @@ func main() {
 		mu.Unlock()
 
 		// Print the connected message
-		fmt.Printf("%s - %s connected\n", timestamp(), clients[conn])
+		fmt.Printf("%s - %s connected\n", timestamp(), nodeName)
+
+		//Create logging file
+		var log_err error
+		logFile, log_err = os.Create("logFile.txt") // This will create a new file or truncate an existing one
+		if log_err != nil {
+			fmt.Println("Error creating file:", err)
+			return
+		}
+		defer logFile.Close()
+
+		// Writing a file title
+		_, log_title_err := logFile.WriteString("Logging at time " + timestamp() + "\n")
+		if log_title_err != nil {
+			fmt.Println("Error writing log file tilte:", err)
+			return
+		}
 
 		// Handle each connection concurrently
 		go handleConnection(conn)
